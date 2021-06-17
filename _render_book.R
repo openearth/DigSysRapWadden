8db1239dfd1b3043016b9e4e858cd573cc361596
stolte <stolte@cb12c900-8be6-4e96-8018-b3b7238a1979>
2021-06-17 07:10:14 +0000

    included preview chapter statements
    git-svn-id: https://repos.deltares.nl/repos/DigSysRapWadden@12 cb12c900-8be6-4e96-8018-b3b7238a1979

--- _render_book.R
+++ _render_book.R
@@ -5,6 +5,14 @@ rm(list = ls())
 require(rmarkdown)
 require(bookdown)
 
+# to preview a chapter, uncomment one of the lines and run.
+# bookdown::preview_chapter("01_index.Rmd")
+# bookdown::preview_chapter("060_Bathymetrie_en_morfodynamiek.Rmd")
+# bookdown::preview_chapter("20_waterkwaliteit.Rmd")
+# bookdown::preview_chapter("30_zeehonden.Rmd")
+# bookdown::preview_chapter("40_vogels.Rmd")
+# bookdown::preview_chapter("70_waterkwantiteit.Rmd")
+
 # gitbook formatted html pages (gebruikt op testpagina)
 bookdown::render_book("index.Rmd", output_format = NULL, 
                       new_session = T)
