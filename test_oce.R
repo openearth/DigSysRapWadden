646e39b80f860a7cfe17721a003776c050329501
stolte <stolte@cb12c900-8be6-4e96-8018-b3b7238a1979>
2021-07-21 17:53:53 +0000

    test file aangepast voor harmonics berekening
    git-svn-id: https://repos.deltares.nl/repos/DigSysRapWadden@43 cb12c900-8be6-4e96-8018-b3b7238a1979

--- test_oce.R
+++ test_oce.R
@@ -26,7 +26,7 @@ ggplot(d3, aes()) +
   geom_point(aes(tijdstip, i.numeriekewaarde - numeriekewaarde, color = opzet), size = 0.4)
 
 
-# oce package voor oceanographic analysis
+#==== tidal harmonics using oce package =================================
 
 require(oce)
 
@@ -52,85 +52,24 @@ m@data$amplitude
 m@data$phase
 
 
-# voor alle data (nog testen)
+#===== package TideHarmonics ===================
+#
 
-require(data.table)
-require(oce)
-require(lubridate)
-require(tidyverse)
-filenamesRaw = list.files(file.path(datadir, "ddl/raw/waterhoogte"), full.names = T, recursive = T)
+require(TideHarmonics)
 
+f1 = file.path(datadir, "ddl/raw/waterhoogte/SCHIERMNOG_OW_WATHTBRKD_NVT_NAP_2017_ddl_wq.csv")
+f2 = file.path(datadir, "ddl/raw/waterhoogte/SCHIERMNOG_OW_WATHTE_NVT_NAP_2017_ddl_wq.csv")
 
-# locs = unique(ophaalCatalogus$locatie.code)
-# filenamesRaw = list.files(file.path(datadir, "ddl/raw/fysisch"), full.names = T, recursive = T, pattern = )
+d1 <- fread(f1, na.strings = c("-999999999", "999999999"))[,list(locatie.code, tijdstip, statuswaarde, grootheid.code, hoedanigheid.omschrijving, eenheid.code, numeriekewaarde)]
+d2 <- fread(f2, na.strings = c("-999999999", "999999999"))[,list(locatie.code, tijdstip, statuswaarde, grootheid.code, hoedanigheid.omschrijving, eenheid.code, numeriekewaarde)]
 
-# for(loc in locs){
-# loc = locs[1]
-fn1 <- filenamesRaw[grepl(paste("OW", "WATHTE", sep = "_"), filenamesRaw)]
-fn2 <- filenamesRaw[grepl(paste("OW", "WATHTBRKD", sep = "_"), filenamesRaw)]
-d1 <- rbindlist(lapply(fn1, function(x) fread(x, na.strings = c("-999999999", "999999999"))[,list(locatie.code, tijdstip, statuswaarde, grootheid.code, hoedanigheid.code, eenheid.code, numeriekewaarde)]))
-d2 <- rbindlist(lapply(fn2, function(x) fread(x, na.strings = c("-999999999", "999999999"))[,list(locatie.code, tijdstip, statuswaarde, grootheid.code, hoedanigheid.code, eenheid.code, numeriekewaarde)]))
-setkey(d1, "tijdstip", "locatie.code", "eenheid.code")
-setkey(d2, "tijdstip", "locatie.code", "eenheid.code")
-d3 <- d1[d2]
-rm(d1, d2)
-d3$numeriekewaarde <- oce::fillGap(d3$numeriekewaarde)
+Tides::gapsts(d1$tijdstip, 10)
+# no gaps
 
-# sl <- oce::as.sealevel(elevation = d3$numeriekewaarde, time = d3$tijdstip, stationName = d3$locatie.code)
-# plot(sl)
+d1 <- d1 %>% arrange(tijdstip)
 
-# d3$opzet <- data.table::fcase(
-#   d3$i.numeriekewaarde - d3$numeriekewaarde < 50, "laag",
-#   d3$i.numeriekewaarde - d3$numeriekewaarde >= 50, "hoog"
-# )
-
-d3[locatie.code == "DELFZL"] %>% 
-  mutate(year = year(tijdstip)) %>%
-  group_by(year, hoedanigheid.code, i.hoedanigheid.code, eenheid.code) %>% summarize(n = n()) %>% View()
+TideHarmonics::ftide(d1$numeriekewaarde, d1$tijdstip, hcn = TideHarmonics::hc60)
 
 
-monthlyMax <- d3[hoedanigheid.code == "NAP", .(
-  opzet = numeriekewaarde - i.numeriekewaarde,
-  month = lubridate::month(tijdstip) , 
-  year = lubridate::year(tijdstip),
-  station = locatie.code
-)][, .(
-  station = station,
-  max = max(opzet),
-  p99 = quantile(opzet, 0.99)
-),
-by = list(year, month, station)][,.(
-  station, max, p99, datum = as.Date(paste(year, month, "15", sep = "-"))),
-]
-
-# boxplot per year based on monthly max
-ggplot(monthlyMax, aes(datum, max)) +
-  geom_boxplot(aes(group = year(datum))) +
-  # geom_smooth(method = "lm", aes(color = station), alpha = 0) +
-  geom_point(aes(color = station), alpha = 0.2) +
-  coord_cartesian(ylim = c(0, 450)) +
-  facet_wrap( ~ station)
-
-
-yearlyMax <- d3[hoedanigheid.code == "NAP", .(
-  opzet = numeriekewaarde - i.numeriekewaarde,
-  # month = lubridate::month(tijdstip) , 
-  year = lubridate::year(tijdstip),
-  station = locatie.code
-)][, .(
-  station = station,
-  max = max(opzet),
-  p99 = quantile(opzet, 0.99)
-),
-by = list(year, station)][,.(
-  station, max, p99, year),
-]
-
-ggplot(yearlyMax, aes(year, p99)) +
-  # geom_point(aes(color = station)) +
-  geom_ribbon(aes(ymin = p99, ymax = max)) + 
-  # geom_smooth(method = "loess", aes(color = station), alpha = 0) +
-  coord_cartesian(ylim = c(30, 350)) +
-  facet_grid(station ~ .)
 
 
