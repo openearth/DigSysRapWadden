a781babb7f7e431fb3daf13b5c425bf9d0827c9f
stolte <stolte@cb12c900-8be6-4e96-8018-b3b7238a1979>
2021-10-27 09:29:05 +0000

    kleine veranderingen in tekst en figuurformaten
    git-svn-id: https://repos.deltares.nl/repos/DigSysRapWadden@93 cb12c900-8be6-4e96-8018-b3b7238a1979

--- _render_book.R
+++ _render_book.R
@@ -9,6 +9,8 @@ require(bookdown)
 # bookdown::preview_chapter("index.Rmd")
 # bookdown::preview_chapter("10_meteorologieenklimaat.Rmd")
 # bookdown::preview_chapter("060_Bathymetrie_en_morfodynamiek.Rmd")
+# bookdown::preview_chapter("10_meteorologieenklimaat.Rmd")
+# bookdown::preview_chapter("060_Bathymetrie_en_morfodynamiek.Rmd")
 # bookdown::preview_chapter("20_waterkwaliteit.Rmd")
 # bookdown::preview_chapter("30_zeehonden.Rmd")
 # bookdown::preview_chapter("40_vogels.Rmd")
@@ -23,7 +25,7 @@ bookdown::render_book("index.Rmd", output_format = NULL,
                       new_session = F)
 # "normal" pdf
 # options(tinytex.verbose = FALSE) # change to TRUE for debugging
-bookdown::render_book("index.Rmd", output_format = bookdown::pdf_book(latex_engine = "xelatex"),
+bookdown::render_book("index.Rmd", output_format = bookdown::pdf_book(latex_engine = "lualatex"),
                       new_session = T, clean_envir = T)
 
 # Veel gemaakte fouten
