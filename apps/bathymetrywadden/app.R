cbf01957f911b5a2183872e2f82c5e218b6c93dc
stolte <stolte@cb12c900-8be6-4e96-8018-b3b7238a1979>
2021-12-17 19:43:52 +0000

    Zoom level is kept when changing years.
    git-svn-id: https://repos.deltares.nl/repos/DigSysRapWadden@111 cb12c900-8be6-4e96-8018-b3b7238a1979

--- apps/bathymetrywadden/app.R
+++ apps/bathymetrywadden/app.R
@@ -8,12 +8,8 @@ library(httr)
 library(jsonlite)
 library(sf)
 
-
-
-
 poly <- sf::st_read(file.path("https://watersysteemdata.deltares.nl/thredds/fileServer/watersysteemdata/Wadden/RWS/bathymetrie/FriescheZeegat-P-Z.geojson"), quiet = T) %>%
     st_transform(4326)
-
 
 ui <- fluidPage(title = "Wadden Sea Bathymetry",
                 tags$style("
@@ -26,62 +22,95 @@ ui <- fluidPage(title = "Wadden Sea Bathymetry",
           opacity: 1;
         }
                "),
-                     fluidRow(
-                         column(width = 12,
-                             # to fill full height of page
-                             tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
-                             leafletOutput("map")
-                         ),
-                         absolutePanel(id = "controls", class = "panel panel-default",
-                                       top = "5%", left = "5%", width = "600px",
-                                       draggable = T,
-                                       tags$style(type = 'text/css', '#big_slider .irs-grid-text {font-size: 14px}'), 
-                                       div(id = 'big_slider',
-                                           sliderInput("endyear", "Jaar", 1926, 2020, c(2005,2020), sep = "", dragRange = TRUE, width = "500px"),
-                                           shiny::checkboxInput("hillshade", "hillshade", TRUE)
-                                       )#div close,
-                         )
-                     )
+                fluidRow(
+                    column(width = 12,
+                           # to fill full height of page
+                           tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+                           leafletOutput("map")
+                    ),
+                    absolutePanel(id = "controls", class = "panel panel-default",
+                                  top = "5%", left = "5%", width = "600px",
+                                  draggable = T,
+                                  tags$style(type = 'text/css', '#big_slider .irs-grid-text {font-size: 14px}'), 
+                                  div(id = 'big_slider',
+                                      sliderInput("endyear", "Jaar", 1926, 2020, c(2005,2020), sep = "", dragRange = TRUE, width = "500px"),
+                                      shiny::checkboxInput("hillshade", "hillshade", TRUE)
+                                  )#div close,
+                    )
+                )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$map <- renderLeaflet({
-        request <- list(dataset = "vaklodingen", 
-                        begin_date = paste0(as.integer(input$endyear[1])-0, "-01-01T00:00:00.000Z"), 
-                        end_date = paste0(input$endyear[2], "-12-31T23:59:00.000Z"), 
-                        min = -3000L, 
-                        max = 1000L, 
-                        hillshade = input$hillshade)
-        jsonInput2012 <- toJSON(request, auto_unbox = T)
-        # run request
-        res <- POST(
+    
+    
+    request <- reactive({list(dataset = "vaklodingen", 
+                              begin_date = paste0(as.integer(input$endyear[1])-0, "-01-01T00:00:00.000Z"), 
+                              end_date = paste0(input$endyear[2], "-12-31T23:59:00.000Z"), 
+                              min = -3000L, 
+                              max = 1000L, 
+                              hillshade = input$hillshade)
+    })
+    
+    jsonInput <- reactive(toJSON(request(), auto_unbox = T))
+    
+    res <- reactive({
+        POST(
             "https://hydro-engine.ey.r.appspot.com//get_bathymetry", 
-            body = jsonInput2012, 
+            body = jsonInput(), 
             encode = "form", 
             verbose(), 
             content_type("application/json")
         )
-        # form map on a background
-        leaflet() %>%
+    })
+    
+    
+    output$map = renderLeaflet({
+        
+        
+        # run request
+        
+        leaflet() %>% 
             addTiles(group = "OSM") %>%
             addProviderTiles(provider = "Esri.WorldImagery", group = "ESRI worldimagery") %>%
-            addTiles("http://{s}.{z}/{x}/{y}.png", group = "bathymetrie") %>%
-            # addTiles("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") %>%
-            addTiles(content(res)$url) %>% setView(5.0, 53.0, zoom = 12) %>%
-            addPolygons(data = poly, group = "polygons") %>%
+            setView(5.0, 53.0, zoom = 12) 
+    })
+    
+    
+    # output$map <- renderLeaflet({
+    observe({
+        
+        leafletProxy("map") %>%
+            clearShapes() %>%
+            addTiles(content(res())$url, group = "bathymetrie") %>%
+            addPolygons(data = poly, group = "vakken") %>%
             leaflet::addLayersControl(
                 baseGroups = c("OSM", "ESRI worldimagery"), 
-                overlayGroups = c("polygons"),
+                overlayGroups = c("vakken", "bathymetrie"),
                 options = layersControlOptions(noHide = T, collapsed = FALSE),
-                ) %>%
+            ) #%>%
+        # leaflet::addLegend(
+        #     colors = unlist(strsplit(content(res())$palette, ",")),
+        #     labels = round(seq(request()$min/100, request()$max/100, length.out = 20), 0), opacity = 1
+        # )
+    })
+    
+    # Use a separate observer to recreate the legend as needed.
+    observe({
+        proxy <- leafletProxy("map")
+        
+        # Remove any existing legend, and only if the legend is
+        # enabled, create a new one.
+        proxy %>% 
+            clearControls() %>%
             leaflet::addLegend(
-                colors = unlist(strsplit(content(res)$palette, ",")), 
-                labels = round(seq(request$min/100, request$max/100, length.out = 20), 0), opacity = 1
+                colors = unlist(strsplit(content(res())$palette, ",")),
+                labels = round(seq(request()$min/100, request()$max/100, length.out = 20), 0), opacity = 1
             )
     })
-
+    
+    
+    
     # keep zooming level when input changes
     # after e.g. https://stackoverflow.com/questions/48397262/in-shiny-how-to-fix-lock-leaflet-map-view-zoom-and-center
     
@@ -91,7 +120,7 @@ server <- function(input, output) {
     
     
     
-    }
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
