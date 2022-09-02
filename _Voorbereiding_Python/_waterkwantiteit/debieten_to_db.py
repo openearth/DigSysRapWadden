cee806e16fbd10fe1afbbcfaf65781e2bb2b1a37
dees <dees@cb12c900-8be6-4e96-8018-b3b7238a1979>
2022-09-02 12:31:05 +0000

    git-svn-id: https://repos.deltares.nl/repos/DigSysRapWadden@189 cb12c900-8be6-4e96-8018-b3b7238a1979

---
+++ _Voorbereiding_Python/_waterkwantiteit/debieten_to_db.py
@@ -0,0 +1,54 @@
+#duitse debieten P:\11202493--systeemrap-grevelingen\1_data\Wadden\NLWKN\afvoeren\standard
+#bewerking om op te slaan in database
+import configparser
+from sqlalchemy import create_engine, MetaData
+import pandas as pd
+import os
+
+#make connection to db 
+cf = configparser.ConfigParser()
+
+cf.read(r'C:\projecten\rws\2022\zoetwaterdebiet\connection_online.txt')
+
+host = cf.get('Postgis', 'host')
+
+connstr = 'postgresql+psycopg2://'+cf.get('Postgis','user')+':'+cf.get('Postgis','pass')+'@'+cf.get('Postgis','host')+':5432/'+cf.get('Postgis','db')
+engine = create_engine(connstr,echo=False)
+
+#read friesland file
+path=r'P:\11202493--systeemrap-grevelingen\1_data\Wadden\Wetterskip\standard\friesland_debieten.csv'
+
+""" ----- inlezen van csv in een dataframe""" 
+df = pd.read_csv(path, delimiter=' ', low_memory=False)
+
+""" ----- converteren van dataframe naar database """ 
+aschema = 'zoetwaterafvoeren'
+df.to_sql('friesland', engine,schema=aschema,if_exists='replace')
+
+""" ----- aanmaken colom met geometrie (in dit geval gebaseerd op lat lon coordinaten, check dit in de csv, welke EPSG/CRS dit is """ 
+strSql = """alter table {s}.friesland add geom geometry(POINT,28992)""".format(s=aschema)  # RD_new (28992)
+engine.execute(strSql)
+
+""" ----- vullen van de kolom, gebaseerd op de velden lon en lat """ 
+strSql = """update {s}.friesland set geom = st_setsrid(st_point(longitude,latitude),28992)""".format(s=aschema)
+engine.execute(strSql)
+
+#duitse debieten naar db
+dir=r'P:\11202493--systeemrap-grevelingen\1_data\Wadden\NLWKN\afvoeren\standard'
+
+    # iterating over all files
+for files in os.listdir(dir):
+    if files.endswith('.csv'): #search for files in folder with the csv extension
+        df = pd.read_csv(os.path.join(dir, files), delimiter=' ', low_memory=False)
+        f = os.path.join(dir, files)
+        name=os.path.splitext(files)[0]
+        name=name.replace('-','_')
+        name=name.lower()
+
+        df.to_sql(name, engine,schema=aschema,if_exists='replace')
+
+        strSql = """alter table {s}.{t} add geom geometry(POINT,4326)""".format(s=aschema, t=name)  # RD_new (28992)
+        engine.execute(strSql)
+
+        strSql = """update {s}.{t} set geom = st_setsrid(st_point(longitude,latitude),4326)""".format(s=aschema, t=name)
+        engine.execute(strSql)
---
+++ _Voorbereiding_Python/_waterkwantiteit/friesland_debieten.py
@@ -0,0 +1,87 @@
+#read in friesland debieten
+#debieten zijn miljoen m3 per etmaal, worden omgerekend naar m3 per seconde
+#processing to dataformat of eems
+from datetime import date
+import pandas as pd
+import numpy as np
+
+debieten = pd.read_excel("C:\\projecten\\rws\\2022\zoetwaterdebiet\\Debieten_friesland.xlsx", header=2)
+debieten['Datum']=pd.to_datetime(debieten['Datum'], format='%Y-%m-%d')
+
+#replace ----- with zeros as they are no measurements
+debieten=debieten.replace('-----', np.NaN)
+debieten=debieten.dropna()
+
+xy_harlingen=[157210,576940]
+xy_ropta=[158487,580303]
+xy_hgmiedema=[170898,591438]
+
+debieten['Harlingen']=1000000*debieten['Harlingen'].div(24*60)
+debieten[ 'Ropta']=1000000*debieten['Ropta'].div(24*60)
+debieten['H.G. Miedema']=1000000*debieten['H.G. Miedema'].div(24*60)
+
+#make new df and drop nans
+h=debieten[['Datum', 'Harlingen']].copy().dropna()
+r=debieten[['Datum', 'Ropta']].copy().dropna()
+m=debieten[['Datum', 'H.G. Miedema']].copy().dropna()
+"""
+datum locatie.origineel numeriekewaarde waardebewerkingsmethode.omschrijving grootheid.omschrijving grootheid.code eenheid.code longitude latitude locatie.naam gebied
+"""
+
+h = h.join(pd.DataFrame(
+    {
+        'locatie.origineel': 'Harlingen',
+        'waardebewerkingsmethode.omschrijving': 'Etmaalgemiddelde',
+        'grootheid.omschrijving': 'Debiet',
+        'grootheid.code': 'Q', 
+        'eenheid.code':'m3/s',
+        'longitude': xy_harlingen[0],
+        'latitude':xy_harlingen[1],
+        'locatie.naam':'Harlingen',
+        'gebied':'Friesland'
+    }, index=h.index
+))
+
+h.columns = h.columns.str.replace('Harlingen','numeriekewaarde')
+h.columns = h.columns.str.replace('Datum','datum')
+
+r = r.join(pd.DataFrame(
+    {
+        'locatie.origineel': 'Ropta',
+        'waardebewerkingsmethode.omschrijving': 'Etmaalgemiddelde',
+        'grootheid.omschrijving': 'Debiet',
+        'grootheid.code': 'Q', 
+        'eenheid.code':'m3/s',
+        'longitude': xy_ropta[0],
+        'latitude':xy_ropta[1],
+        'locatie.naam':'Ropta',
+        'gebied':'Friesland'
+    }, index=r.index
+))
+
+r.columns = r.columns.str.replace('Ropta','numeriekewaarde')
+r.columns = r.columns.str.replace('Datum','datum')
+
+m = m.join(pd.DataFrame(
+    {
+        'locatie.origineel': 'H.G. Miedema',
+        'waardebewerkingsmethode.omschrijving': 'Etmaalgemiddelde',
+        'grootheid.omschrijving': 'Debiet',
+        'grootheid.code': 'Q', 
+        'eenheid.code':'m3/s',
+        'longitude': xy_hgmiedema[0],
+        'latitude':xy_hgmiedema[1],
+        'locatie.naam':'H.G. Miedema',
+        'gebied':'Friesland'
+    }, index=m.index
+))
+
+m.columns = m.columns.str.replace('H.G. Miedema','numeriekewaarde',regex=True)
+m.columns = m.columns.str.replace('Datum','datum')
+
+all=pd.concat([h,r,m])
+all=all.sort_values(by='datum')
+
+
+all.to_csv(r'P:\11202493--systeemrap-grevelingen\1_data\Wadden\Wetterskip\standard\friesland_debieten.csv', sep=' ', index=False)
+
