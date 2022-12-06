1bf5ef976a72167c31a04640185a8d40fec04e49
dees <dees@cb12c900-8be6-4e96-8018-b3b7238a1979>
2022-12-06 08:46:57 +0000

    git-svn-id: https://repos.deltares.nl/repos/DigSysRapWadden@214 cb12c900-8be6-4e96-8018-b3b7238a1979

--- _Voorbereiding_Python/_waterkwantiteit/debieten_to_db.py
+++ _Voorbereiding_Python/_waterkwantiteit/debieten_to_db.py
@@ -78,3 +78,20 @@ for files in os.listdir(dir):
 
         strSql = """update {s}.{t} set geom = st_setsrid(st_point(longitude,latitude),4326)""".format(s=aschema, t=name)
         engine.execute(strSql)
+
+# %% binnenwateren naar database
+path = r'p:\11202493--systeemrap-grevelingen\1_data\Wadden\RWS\afvoeren\standard\etmaalgemiddelden.csv'
+
+df = pd.read_csv(path, delimiter=';', low_memory=False)
+""" ----- converteren van dataframe naar database """ 
+aschema = 'zoetwaterafvoeren'
+df.to_sql('afvoeren_rijkswateren', engine,schema=aschema,if_exists='replace')
+
+""" ----- aanmaken colom met geometrie (in dit geval gebaseerd op lat lon coordinaten, check dit in de csv, welke EPSG/CRS dit is """ 
+strSql = """alter table {s}.afvoeren_rijkswateren add geom geometry(POINT,28992)""".format(s=aschema)  # RD_new (28992)
+engine.execute(strSql)
+
+""" ----- vullen van de kolom, gebaseerd op de velden lon en lat """ 
+strSql = """update {s}.afvoeren_rijkswateren set geom = st_setsrid(st_point("geometriepunt.x","geometriepunt.y"),28992)""".format(s=aschema)
+engine.execute(strSql)
+# %%
