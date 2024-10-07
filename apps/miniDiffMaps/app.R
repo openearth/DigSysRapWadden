20e6f129ce1d6e74dba97696191c6d316d5959ff
stolte <stolte@cb12c900-8be6-4e96-8018-b3b7238a1979>
2024-10-07 11:11:17 +0000

    appending new diff maps
    git-svn-id: https://repos.deltares.nl/repos/DigSysRapWadden@319 cb12c900-8be6-4e96-8018-b3b7238a1979

--- apps/miniDiffMaps/app.R
+++ apps/miniDiffMaps/app.R
@@ -16,37 +16,39 @@ CSS <- "
 }
 "
 
-#=== read data =================================================================
+#=== Preparation ======= run once ============================================
 
-# diffMapDir <- "p:/11202493--systeemrap-grevelingen/1_data/Wadden/RWS/bathymetrie/processing_tiles/volledige_bodemkaarten_BenO_Wadden/verschilkaarten_nieuwedata/"
+prepare = FALSE
 
+if(prepare) {
+  diffMapDirRDS <- "apps/miniDiffMaps/data"
+  diffMapDirPrep <- "p:/11202493--systeemrap-grevelingen/1_data/Wadden/RWS/bathymetrie/processing_tiles/volledige_bodemkaarten_BenO_Wadden/morphopy/verschilkaarten/"
+  
+  filesprep <- list.files(diffMapDirPrep, pattern = "tiff", full.names = TRUE)
+  
+  diffMaps <- lapply(
+    filesprep,
+    function(x){
+      terra::rast(
+        file.path(diffMapDirPrep, x)
+      ) %>%
+        terra::aggregate(2) %>%
+        terra::mask(wadden) %>%
+        terra::project("epsg:4326") %>%
+        saveRDS(file = file.path(diffMapDirRDS, paste0(sub(".tiff", "", x),  ".RDS")))
+    }
+  )
+}
+
+## only if "vakken" need to be showed in app
 # wadden <- sf::read_sf("https://datahuiswadden.openearth.nl/geoserver/dhw/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=dhw%3Akombergingen&maxFeatures=50&outputFormat=application%2Fjson") %>%
 #   dplyr::select(id) %>%
 #   sf::st_transform(28992) %>%
 #   sf::st_simplify()
 
 diffMapDir <- "data"
-
 files <- list.files(diffMapDir, pattern = ".RDS")
-
 diffYears <- sub(".RDS", "", files)
-
-
-#=== Preparation ===============================================================
-
-# diffMaps <- lapply(
-#   files,
-#   function(x){
-#     terra::rast(
-#       file.path(diffMapDir, x)
-#     ) %>%
-#       terra::aggregate(2) %>%
-#       terra::mask(wadden) %>%
-#       terra::project("epsg:4326") %>%
-#     saveRDS(file = file.path(diffMapDir, paste0(sub(".tiff", "", x),  ".RDS")))
-#   }
-# )
-
 
 #=== user interface ============================================================
 
@@ -82,43 +84,26 @@ ui <- miniPage(
 #=== Server ====================================================================
 
 server <- function(input, output, session) {
-
-  #=== initialize map ============================================================
-  output$diffplot = renderLeaflet({
-    leaflet() %>% 
-      setView(5.0, 53.0, zoom = 12) 
-  })
   
-    maxScale <- reactiveValues()
+  maxScale <- reactiveValues()
   
-  observeEvent(
-    c(
-      input$btn
-    ),{
-      maxScale$values <- input$maxScale
-    }, 
-    ignoreNULL = FALSE)
+  observeEvent(c(input$btn),{
+    maxScale$values <- input$maxScale
+  }, 
+  ignoreNULL = FALSE)
   
-  
-    
-#=== open the appropriate diff raster ==========================================
+  output$diffPlot <- renderLeaflet({
     
     diff <- terra::rast(
       file.path(diffMapDir, paste0(input$periode, ".RDS"))
     )
-
-#=== colors for plot raster ====================================================
+    
+    #=== colors for plot raster ====================================================
     
     pal <- colorNumeric(c("red", "white", "blue"), c(-maxScale$values, maxScale$values),
                         na.color = "transparent")
     
-#=== produce map
-    
-  # output$diffPlot <- renderLeaflet({
-  observe({
-    leafletProxy("map") %>%
-      clearTiles() %>%
-      # leaflet::leaflet() %>%
+    leaflet::leaflet() %>%
       leaflet::addTiles(group = "OpenStreetMap") %>%
       addProviderTiles(provider = "Esri.WorldImagery", group = "ESRI worldimagery") %>%
       leaflet::addRasterImage(diff, colors = pal, opacity = 0.6, group = "verschilkaart") %>%
@@ -129,33 +114,6 @@ server <- function(input, output, session) {
         options = layersControlOptions(noHide = T, collapsed = FALSE),
       )
   })
-  
-  # # Use a separate observer to recreate the legend as needed.
-  # observe({
-  #   proxy <- leafletProxy("map")
-  #   
-  #   # Remove any existing legend, and only if the legend is
-  #   # enabled, create a new one.
-  #   proxy %>%
-  #     clearControls() %>%
-  #     leaflet.extras::addWMSLegend(
-  #       uri=paste0(sub("ows", "wms", wms_base),
-  #                  "?request=",
-  #                  "GetLegendGraphic&version=1.3.0&",
-  #                  "format=image/png&layer=",
-  #                  "bathymetrie:",
-  #                  unname(layers[input$endyear])
-  #       )
-  #     )
-  # })
-  
-  # keep zooming level when input changes
-  # after e.g. https://stackoverflow.com/questions/48397262/in-shiny-how-to-fix-lock-leaflet-map-view-zoom-and-center
-  
-  zoom <- reactive({
-    ifelse(is.null(input$map_zoom),3,input$map_zoom)
-  })
-  
 }
 
 # runGadget(ui, server, viewer = paneViewer())
