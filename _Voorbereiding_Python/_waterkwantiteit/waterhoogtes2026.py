b0958d3861095dd2e33c9cb16f0eee6704bdef9a
nathaliedees <64775981+nathaliedees@users.noreply.github.com>
2026-08-25 15:23:03 +0200

    update to run on VM

--- _Voorbereiding_Python/_waterkwantiteit/waterhoogtes2026.py
+++ _Voorbereiding_Python/_waterkwantiteit/waterhoogtes2026.py
@@ -16,7 +16,7 @@ logging.getLogger("ddlpy").setLevel(logging.DEBUG)
 
 #%% TODO write functions and move into jupyter scripts once the functions are there to allow the user for easier data selection and collection
 path = Path.cwd()
-grootheid = ['WATHTE', 'WATHTBRKD']
+grootheid = ['WATHTE']
 save_path = r'P:\11202493--systeemrap-grevelingen\1_data\Wadden\ddl\raw\waterhoogte2026'
 
 define_selection = pd.read_excel(os.path.join(path, 'define_parameter_selection.xlsx'))
@@ -53,66 +53,68 @@ selected_stations.plot()
 # get the dataframe with locations and their available parameters
 locations = ddlpy.locations()
 # Filter the locations dataframe with the desired parameters and stations.
-for i in range(len(selected_stations['CODE'])):
-    bool_stations = locations.index.isin([(selected_stations['CODE'].unique()[i])])
-    # meting/astronomisch/verwachting
-    # need to investigate how it works with multiple parameters
-    bool_procestype = locations["ProcesType"].isin(define_selection['ProcesType'].to_numpy())
-    # waterlevel/waterhoogte (WATHTE)
-    bool_grootheid = locations["Grootheid.Code"].isin(define_selection['Grootheid.Code'].to_numpy())
-    # timeseries ("") versus extremes (GETETM2/GETETMSL2/GETETBRKD2/GETETBRKDMSL2)
-    # bool_groepering = locations["Groepering.Code"].isin(define_selection['Groepering.Code'].to_numpy())
-    # bool_parameter = locations["Parameter.Code"].isin(["Cl"])
-    # vertical reference (NAP/MSL)
-    bool_hoedanigheid = locations["Hoedanigheid.Code"].isin(define_selection['Hoedanigheid.Code'].to_numpy())
-    selected = locations.loc[
-        bool_procestype
-        & bool_stations
-        & bool_grootheid
-        # & bool_parameter
-        # & bool_groepering
-        & bool_hoedanigheid
-        ]
+for station in selected_stations['CODE'].unique():
+    for i in range(len(define_selection)):
+        define_selection_pars = define_selection.iloc[i]
+        bool_stations = locations.index.isin([station])
+        # meting/astronomisch/verwachting
+        # need to investigate how it works with multiple parameters
+        bool_procestype = locations["ProcesType"].isin([define_selection_pars['ProcesType']])
+        # waterlevel/waterhoogte (WATHTE)
+        bool_grootheid = locations["Grootheid.Code"].isin([define_selection_pars['Grootheid.Code']])
+        # timeseries ("") versus extremes (GETETM2/GETETMSL2/GETETBRKD2/GETETBRKDMSL2)
+        # bool_groepering = locations["Groepering.Code"].isin(define_selection['Groepering.Code'].to_numpy())
+        # bool_parameter = locations["Parameter.Code"].isin(["Cl"])
+        # vertical reference (NAP/MSL)
+        bool_hoedanigheid = locations["Hoedanigheid.Code"].isin([define_selection_pars['Hoedanigheid.Code']])
+        selected = locations.loc[
+            bool_procestype
+            & bool_stations
+            & bool_grootheid
+            # & bool_parameter
+            # & bool_groepering
+            & bool_hoedanigheid
+            ]
 
-    start_date = selected_stations['TIJDSTIP_LAATSTE_METING'].min()  # Use the earliest measurement date from the selected stations
-    end_date = selected_stations['TIJDSTIP_LAATSTE_METING'].max()
+        start_date = selected_stations['TIJDSTIP_LAATSTE_METING'].min()  # Use the earliest measurement date from the selected stations
+        end_date = selected_stations['TIJDSTIP_LAATSTE_METING'].max()
 
-    # provide a single row of the locations dataframe to ddlpy.measurements
-    measurements = ddlpy.measurements(selected.iloc[0], start_date=start_date, end_date=end_date)
+        # provide a single row of the locations dataframe to ddlpy.measurements
+        measurements = ddlpy.measurements(selected.iloc[0], start_date=start_date, end_date=end_date)
 
-    if not measurements.empty:
-        print("Data was found in RWS Waterwebservices/DDL")
-        print("data for location:", selected['Naam'].iloc[0])
-        measurements.plot(y="Meetwaarde.Waarde_Numeriek", linewidth=0.8)
-    else:
-        print("No Data!")
+        if not measurements.empty:
+            print("Data was found in RWS Waterwebservices/DDL")
+            print("data for location:", selected['Naam'].iloc[0])
+            measurements.plot(y="Meetwaarde.Waarde_Numeriek", linewidth=0.8)
+        else:
+            print("No Data!")
 
 
-    cols_tokeep = ['WaarnemingMetadata.Bemonsteringshoogte',
-        'WaarnemingMetadata.Kwaliteitswaardecode',
-        'WaarnemingMetadata.OpdrachtgevendeInstantie',
-        'WaarnemingMetadata.Referentievlak', 'WaarnemingMetadata.Statuswaarde',
-        'BemonsteringsApparaat.Code', 'BemonsteringsApparaat.Omschrijving',
-        'BemonsteringsMethode.Code', 'BemonsteringsMethode.Omschrijving',
-        'BemonsteringsSoort.Code', 'BemonsteringsSoort.Omschrijving',
-        'Compartiment.Code',
-        'Compartiment.Omschrijving', 'Eenheid.Code', 'Eenheid.Omschrijving',
-        'Groepering.Code', 'Groepering.Omschrijving', 'Grootheid.Code',
-        'Grootheid.Omschrijving', 'Hoedanigheid.Code',
-        'Hoedanigheid.Omschrijving', 'MeetApparaat.Code',
-        'MeetApparaat.Omschrijving',
-        'Parameter.Code', 'Parameter.Omschrijving',
-        'Parameter_Wat_Omschrijving', 'ProcesType', 'Typering.Code',
-        'Typering.Omschrijving', 'WaardeBepalingsMethode.Code',
-        'WaardeBepalingsMethode.Omschrijving', 'WaardeBepalingsTechniek.Code',
-        'WaardeBepalingsTechniek.Omschrijving', 'WaardeBewerkingsMethode.Code',
-        'WaardeBewerkingsMethode.Omschrijving',
-        'Meetwaarde.Waarde_Alfanumeriek', 'Meetwaarde.Waarde_Numeriek', 'Code',
-        'Coordinatenstelsel', 'Naam', 'Lon', 'Lat']
+        cols_tokeep = ['WaarnemingMetadata.Bemonsteringshoogte',
+            'WaarnemingMetadata.Kwaliteitswaardecode',
+            'WaarnemingMetadata.OpdrachtgevendeInstantie',
+            'WaarnemingMetadata.Referentievlak', 'WaarnemingMetadata.Statuswaarde',
+            'BemonsteringsApparaat.Code', 'BemonsteringsApparaat.Omschrijving',
+            'BemonsteringsMethode.Code', 'BemonsteringsMethode.Omschrijving',
+            'BemonsteringsSoort.Code', 'BemonsteringsSoort.Omschrijving',
+            'Compartiment.Code',
+            'Compartiment.Omschrijving', 'Eenheid.Code', 'Eenheid.Omschrijving',
+            'Groepering.Code', 'Groepering.Omschrijving', 'Grootheid.Code',
+            'Grootheid.Omschrijving', 'Hoedanigheid.Code',
+            'Hoedanigheid.Omschrijving', 'MeetApparaat.Code',
+            'MeetApparaat.Omschrijving',
+            'Parameter.Code', 'Parameter.Omschrijving',
+            'Parameter_Wat_Omschrijving', 'ProcesType', 'Typering.Code',
+            'Typering.Omschrijving', 'WaardeBepalingsMethode.Code',
+            'WaardeBepalingsMethode.Omschrijving', 'WaardeBepalingsTechniek.Code',
+            'WaardeBepalingsTechniek.Omschrijving', 'WaardeBewerkingsMethode.Code',
+            'WaardeBewerkingsMethode.Omschrijving',
+            'Meetwaarde.Waarde_Alfanumeriek', 'Meetwaarde.Waarde_Numeriek', 'Code',
+            'Coordinatenstelsel', 'Naam', 'Lon', 'Lat']
 
-    measurements = measurements[cols_tokeep]
-    save_station = selected_stations['CODE'].iloc[i].replace('.', '_')
-    measurements.to_csv(os.path.join(save_path, save_station + '_' + define_selection['Grootheid.Code'].iloc[0] + '.csv'))
+        measurements = measurements[cols_tokeep]
+        save_station = station.replace('.', '_')
+        measurements.to_csv(os.path.join(save_path, save_station + '_' + define_selection_pars['Grootheid.Code'] + '_' + define_selection_pars['ProcesType'] + '.csv'))
 
 
 
