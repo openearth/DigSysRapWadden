6fda7cb80959c60aa31dfc1b0c519e6bee4f3a05
nathaliedees <64775981+nathaliedees@users.noreply.github.com>
2026-09-10 10:35:54 +0200

    Update waterhoogtes2026.py

--- _Voorbereiding_Python/waterhoogte2026/waterhoogtes2026.py
+++ _Voorbereiding_Python/waterhoogte2026/waterhoogtes2026.py
@@ -7,12 +7,6 @@
 #Getijslag (difference GHW and GLW)
 #above can be done in the R code directly using the data fetched from the services, use the 'GETETM2' data to do so
 
-# tidal components - have been derived
-#assymetry has not been derived  #TODO integrating hatyan tidal asymmetry scripts
-
-#- Gemiddeld LaagLaag water bij Springtij #TODO integrating hatyan tidal indicators scripts
-# - Gemiddeld HoogHoog water bij springtij #TODO integrating hatyan tidal indicators scripts
-
 # opslaan per jaar
 # gebruik de parameter wat omschrijving om dit te doen
 # gebruik gelijk de extremen 
@@ -37,23 +31,13 @@ import logging
 logging.basicConfig()
 logging.getLogger("ddlpy").setLevel(logging.DEBUG)
 
-const_list = hatyan_core.get_const_list_hatyan(listtype='year')
-const_list = const_list + ['SSA'] #['A0','M2','S2','M4'] # TODO: add SSA and maybe other components for better reproduction
 #%% TODO write functions and move into jupyter scripts once the functions are there to allow the user for easier data selection and collection
 path = Path.cwd()
-grootheid = ['WATHTE']
 save_path = r'P:\11202493--systeemrap-grevelingen\1_data\Wadden\ddl\raw\waterhoogte2026'
 Path(save_path).mkdir(parents=True, exist_ok=True)
 
-dir_TA_filtersurge = os.path.join(save_path,'TA_filtersurge')
-if not os.path.exists(dir_TA_filtersurge):
-    os.mkdir(dir_TA_filtersurge)
-dir_TA_perstation = os.path.join(save_path,'waterstand_berekend_m')
-if not os.path.exists(dir_TA_perstation):
-    os.mkdir(dir_TA_perstation)
-
 define_selection = pd.read_excel(os.path.join(path, 'define_parameter_selection.xlsx'))
-define_selection = define_selection[define_selection['Grootheid.Code'].isin(grootheid)]
+grootheid = define_selection['Grootheid.Code'].unique()
 
 #%% locatielaatstewaarneming -> retrieving stations convert to function
 gdf = get_locatielaatstewaarning()
@@ -164,57 +148,3 @@ for j in range(len(selected_stations['CODE'].unique()[0:1])):
             f"{define_selection_pars['Groepering.Code']}.csv"
         )  ##TODO check if it needs to be stored with timezone information or not
         measurements.to_csv(os.path.join(save_path, output_filename), index=True)
-
-        if define_selection_pars['ProcesType'] == 'meting' and define_selection_pars['Groepering.Code'] != 'GETETM2': 
-            hatyan_df = hatyan.ddlpy_to_hatyan(measurements) #voor hatyan nemen we nu alleen geonctroleerde waarden
-
-            for year, hatyan_df_year in hatyan_df.groupby(hatyan_df.index.year):
-
-                if hatyan_df_year.empty:
-                    continue
-
-                timestep_min_all = ((hatyan_df_year.index[1:]-hatyan_df_year.index[:-1]).total_seconds()/60).astype(int).values
-                uniq_vals, uniq_counts = np.unique(timestep_min_all,return_counts=True)
-                timestep_min_dominant = uniq_vals[np.argmax(uniq_counts)]
-
-                #resample if timestep is not constant
-                if (hatyan_df_year.index.min() < pd.Timestamp(year-1,12,31,23,0,tz='UTC')) or (hatyan_df_year.index.max() > pd.Timestamp(year+1,1,1,1,0,tz='UTC')):
-                    raise Exception('ERROR: start/stoptimes are not within expected range of year + 1 hour ath both sides')
-                if len(uniq_vals)==1: # no resampling necessary if constant timestep
-                    if timestep_min_dominant not in [10,60,180]:
-                        raise Exception(f'ERROR: constant timestep of {timestep_min_dominant} min, while 10, 60 or 180 min is expected.')
-                    ts_meas = hatyan_df_year.copy()
-                else: #varying timestep, for instance in interval-transition year
-                    if 180 in uniq_vals: #if 180 min interval occurs, resample to 180 min
-                        ts_meas = hatyan.resample_timeseries(ts=hatyan_df_year, timestep_min=180, tstart=hatyan_df_year.index.min(), tstop=hatyan_df_year.index.max())
-                    else: #otherwise resample to 60 minutes
-                        ts_meas = hatyan.resample_timeseries(ts=hatyan_df_year, timestep_min=60, tstart=hatyan_df_year.index.min(), tstop=hatyan_df_year.index.max())
-                try:
-                    comp_frommeas = hatyan.analysis(ts_meas, const_list=const_list, nodalfactors=True, xfac=True, fu_alltimes=True)
-                    print(f"Year {year}:")
-                    print(ts_meas)
-                except Exception as e:
-                    print(f"Error during hatyan analysis for year {year}:", e)
-
-                data_pd_TA_station = pd.DataFrame({}, columns=pd.MultiIndex.from_product([['A','phi_deg'],hatyan_df_year]), index=const_list) 
-
-                comp_frommeas.to_csv(os.path.join(dir_TA_filtersurge,f'{station}_{year}_components_UTC+1.csv'),float_format='%.3f')
-                # data_pd_TA_station.loc[const_list,(['A','phi_deg'],year)] = ts_meas.loc[const_list,['A','phi_deg']].values
-
-                ts_pred = hatyan.prediction(comp=comp_frommeas,times=slice(dt.datetime(year,1,1),dt.datetime(year,12,31,23,50))) #removal of timestep, was previously timestep_min
-                ts_pred.rename_axis('time').to_csv(os.path.join(dir_TA_perstation,f'tspred_anasameyear_{station}_OW_WATHTASTRO_{year}.csv'))
-
-                            #make figure to check the raw data of the files that cannot be used for the tidal analysis
-                fig,(ax1,ax2) = hatyan.plot_timeseries(ts=hatyan_df_year,ts_validation=ts_meas) 
-                ax1.set_title(f'waterlevel measured raw vs filtered for {station} {year}')
-                ax2.set_ylim(-1,1)
-                fig.savefig(os.path.join(dir_TA_filtersurge,f'tsmeas_{station}_{year}.png'))
-
-                fig,(ax1,ax2) = hatyan.plot_timeseries(ts=ts_meas,ts_validation=ts_pred)
-                ax2.set_ylim(-1,1)
-                ax1.set_title(f'tidal prediction vs measured for {station} {year}')
-                fig.savefig(os.path.join(dir_TA_filtersurge,f'tspred_{station}_{year}.png'))
-
-
-  # %%
-hatyan.analysis_prediction.analysis()
